9e3f2b63223b14156cea5667f4f1368cc189865c
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2025-12-16 20:17:45 +0000

    newJobPost

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  let jobTitle = 'Teacher';
  let city = 'New York City';
 
-let NewJobPost = `Hello ${firstName},
+let newJobPost = `Hello ${firstName},
 We have recieved your application for the position of ${jobTitle} in ${city}.
 We will be in touch soon. 
 Sincerely,
@@ -10,7 +10,7 @@ HR manager`;
  
  
 
-document.getElementById("result").innerHTML = NewJobPost;
+document.getElementById("result").innerHTML = newJobPost;
 
 
 
