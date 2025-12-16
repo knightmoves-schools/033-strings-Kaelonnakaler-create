ef67a1190a95e3ac272d941f2e5e83b66ef3bef2
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2025-12-16 20:15:53 +0000

    try this

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
- Firstname = FirstName;
- JobTitle = JobTitle;
- City = City;
+ let firstName = 'Hannah';
+ let jobTitle = 'Teacher';
+ let city = 'New York City';
 
- jobtitle = NewJobPost;
- firstname = NewJobPost;
- NewJobPost = city;
+let NewJobPost = `Hello ${firstName},
+We have recieved your application for the position of ${jobTitle} in ${city}.
+We will be in touch soon. 
+Sincerely,
+HR manager`;
  
  
 
-document.getElementById("NewJobPost").innerHTML = result;
+document.getElementById("result").innerHTML = NewJobPost;
 
 
 
